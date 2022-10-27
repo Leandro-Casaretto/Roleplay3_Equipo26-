fc04dfa8b8b70fb4499fe06750259188c0314fc9
Leandro-Casaretto <leandrodcasaretto@gmail.com>
2022-10-27 20:43:07 -0300

    Cambio en recuperarVida

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -1,11 +1,32 @@
 ﻿using System;
+using RoleplayGame;
 
 namespace Program
 {
     class Program
     {
-        static void Main(string[] args) //comentario2
+        static void Main(string[] args)
         {
+            LibroDeHechizos libro = new LibroDeHechizos();
+            libro.Hechizos = new Hechizo[]{ new Hechizo() };
+
+            Mago pedro = new Mago("Pedro");
+            pedro.BaculoHelado = new BaculoHelado();
+
+            Elfo jose = new Elfo("Jose");
+            jose.ArcoPesado = new ArcoPesado();
+            jose.BotasDeOro = new BotasDeOro();
+
+            Console.WriteLine($"Jose tiene ❤️ {jose.Vida}");
+            Console.WriteLine($"Pedro ataca a Jose con ⚔️ {pedro.valorAtaque}");
+
+            jose.perderVida(pedro);
+
+            Console.WriteLine($"Jose tiene ❤️ {jose.Vida}");
+
+            jose.recuperarVida();
+
+            Console.WriteLine($"Gimli has ❤️ {gimli.Health}");
         }
     }
 }
